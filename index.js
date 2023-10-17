68da66d65086f347b0f94fbe68f8c144f9d17da1
Josie Cisneros <josiecisneros1224@gmail.com>
2023-10-16 20:31:50 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch ("https://anapioficeandfire.com/api/books")
+    .then(function(response) {
+      return response.json()
+  })
+  .then(function(books) {
+    renderBooks(books)
+  })
 }
+
+fetchBooks()
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +22,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
